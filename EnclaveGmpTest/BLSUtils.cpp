952044487078ebe670cf5e279d124ac5fcc395ad
a72d9e2bad9edfd58d6c0248c12c953b71d409d2 <13399135+kladkogex@users.noreply.github.com>
2019-08-15 18:08:16 +0300

    Added files

---
+++ EnclaveGmpTest/BLSUtils.cpp
@@ -0,0 +1,13 @@
+//
+// Created by kladko on 8/14/19.
+//
+
+#define GMP_WITH_SGX
+#include "libff/algebra/curves/alt_bn128/alt_bn128_pp.hpp"
+#include "BLSUtils.h"
+
+void import_key() {
+
+  auto private_key = new libff::alt_bn128_Fr("4160780231445160889237664391382223604184857153814275770598791864649971919844");
+
+}
---
+++ EnclaveGmpTest/BLSUtils.h
@@ -0,0 +1,19 @@
+//
+// Created by kladko on 8/14/19.
+//
+
+#ifndef SGXD_BLSUTILS_H
+#define SGXD_BLSUTILS_H
+
+
+
+#ifdef __cplusplus
+#define EXTERNC extern "C"
+#else
+#define EXTERNC
+#endif
+
+EXTERNC void import_key();
+
+
+#endif //SGXD_BLSUTILS_H
