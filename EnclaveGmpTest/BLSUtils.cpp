3b2afca1f5629fef97870b26e275aa2e4d9ffd40
a72d9e2bad9edfd58d6c0248c12c953b71d409d2 <13399135+kladkogex@users.noreply.github.com>
2019-08-15 18:17:04 +0300

    q :

--- EnclaveGmpTest/BLSUtils.cpp
+++ EnclaveGmpTest/BLSUtils.cpp
@@ -10,4 +10,17 @@ void import_key() {
 
   auto private_key = new libff::alt_bn128_Fr("4160780231445160889237664391382223604184857153814275770598791864649971919844");
 
+
+    mpz_t t;
+    mpz_init(t);
+
+    private_key->as_bigint().to_mpz(t);
+
+    char arr[mpz_sizeinbase (t, 10) + 2];
+
+    char * tmp = mpz_get_str(arr, 10, t);
+    mpz_clear(t);
+
+    std::string output = tmp;
+
 }
