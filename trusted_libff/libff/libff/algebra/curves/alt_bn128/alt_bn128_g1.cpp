ffcd20f0c10bc101dfd0745f1695cb8627a8d43b
a72d9e2bad9edfd58d6c0248c12c953b71d409d2 <13399135+kladkogex@users.noreply.github.com>
2019-09-02 18:52:05 +0300

    Added libff

---
+++ trusted_libff/libff/libff/algebra/curves/alt_bn128/alt_bn128_g1.cpp
@@ -0,0 +1,394 @@
+/** @file
+ *****************************************************************************
+ * @author     This file is part of libff, developed by SCIPR Lab
+ *             and contributors (see AUTHORS).
+ * @copyright  MIT license (see LICENSE file)
+ *****************************************************************************/
+
+#include <libff/algebra/curves/alt_bn128/alt_bn128_g1.hpp>
+
+namespace libff {
+
+#ifdef PROFILE_OP_COUNTS
+long long alt_bn128_G1::add_cnt = 0;
+long long alt_bn128_G1::dbl_cnt = 0;
+#endif
+
+std::vector<size_t> alt_bn128_G1::wnaf_window_table;
+std::vector<size_t> alt_bn128_G1::fixed_base_exp_window_table;
+alt_bn128_G1 alt_bn128_G1::G1_zero;
+alt_bn128_G1 alt_bn128_G1::G1_one;
+
+alt_bn128_G1::alt_bn128_G1()
+{
+    this->X = G1_zero.X;
+    this->Y = G1_zero.Y;
+    this->Z = G1_zero.Z;
+}
+
+void alt_bn128_G1::to_affine_coordinates()
+{
+    if (this->is_zero())
+    {
+        this->X = alt_bn128_Fq::zero();
+        this->Y = alt_bn128_Fq::one();
+        this->Z = alt_bn128_Fq::zero();
+        }
+    else
+    {
+        alt_bn128_Fq Z_inv = Z.inverse();
+        alt_bn128_Fq Z2_inv = Z_inv.squared();
+        alt_bn128_Fq Z3_inv = Z2_inv * Z_inv;
+        this->X = this->X * Z2_inv;
+        this->Y = this->Y * Z3_inv;
+        this->Z = alt_bn128_Fq::one();
+    }
+}
+
+void alt_bn128_G1::to_special()
+{
+    this->to_affine_coordinates();
+}
+
+bool alt_bn128_G1::is_special() const
+{
+    return (this->is_zero() || this->Z == alt_bn128_Fq::one());
+}
+
+bool alt_bn128_G1::is_zero() const
+{
+    return (this->Z.is_zero());
+}
+
+bool alt_bn128_G1::operator==(const alt_bn128_G1 &other) const
+{
+    if (this->is_zero())
+    {
+        return other.is_zero();
+    }
+
+    if (other.is_zero())
+    {
+        return false;
+    }
+
+    /* now neither is O */
+
+    // using Jacobian coordinates so:
+    // (X1:Y1:Z1) = (X2:Y2:Z2)
+    // iff
+    // X1/Z1^2 == X2/Z2^2 and Y1/Z1^3 == Y2/Z2^3
+    // iff
+    // X1 * Z2^2 == X2 * Z1^2 and Y1 * Z2^3 == Y2 * Z1^3
+
+    alt_bn128_Fq Z1_squared = (this->Z).squared();
+    alt_bn128_Fq Z2_squared = (other.Z).squared();
+
+    if ((this->X * Z2_squared) != (other.X * Z1_squared))
+    {
+        return false;
+    }
+
+    alt_bn128_Fq Z1_cubed = (this->Z) * Z1_squared;
+    alt_bn128_Fq Z2_cubed = (other.Z) * Z2_squared;
+
+    if ((this->Y * Z2_cubed) != (other.Y * Z1_cubed))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool alt_bn128_G1::operator!=(const alt_bn128_G1& other) const
+{
+    return !(operator==(other));
+}
+
+alt_bn128_G1 alt_bn128_G1::operator+(const alt_bn128_G1 &other) const
+{
+    // handle special cases having to do with O
+    if (this->is_zero())
+    {
+        return other;
+    }
+
+    if (other.is_zero())
+    {
+        return *this;
+    }
+
+    // no need to handle points of order 2,4
+    // (they cannot exist in a prime-order subgroup)
+
+    // check for doubling case
+
+    // using Jacobian coordinates so:
+    // (X1:Y1:Z1) = (X2:Y2:Z2)
+    // iff
+    // X1/Z1^2 == X2/Z2^2 and Y1/Z1^3 == Y2/Z2^3
+    // iff
+    // X1 * Z2^2 == X2 * Z1^2 and Y1 * Z2^3 == Y2 * Z1^3
+
+    alt_bn128_Fq Z1Z1 = (this->Z).squared();
+    alt_bn128_Fq Z2Z2 = (other.Z).squared();
+
+    alt_bn128_Fq U1 = this->X * Z2Z2;
+    alt_bn128_Fq U2 = other.X * Z1Z1;
+
+    alt_bn128_Fq Z1_cubed = (this->Z) * Z1Z1;
+    alt_bn128_Fq Z2_cubed = (other.Z) * Z2Z2;
+
+    alt_bn128_Fq S1 = (this->Y) * Z2_cubed;      // S1 = Y1 * Z2 * Z2Z2
+    alt_bn128_Fq S2 = (other.Y) * Z1_cubed;      // S2 = Y2 * Z1 * Z1Z1
+
+    if (U1 == U2 && S1 == S2)
+    {
+        // dbl case; nothing of above can be reused
+        return this->dbl();
+    }
+
+    // rest of add case
+    alt_bn128_Fq H = U2 - U1;                            // H = U2-U1
+    alt_bn128_Fq S2_minus_S1 = S2-S1;
+    alt_bn128_Fq I = (H+H).squared();                    // I = (2 * H)^2
+    alt_bn128_Fq J = H * I;                              // J = H * I
+    alt_bn128_Fq r = S2_minus_S1 + S2_minus_S1;          // r = 2 * (S2-S1)
+    alt_bn128_Fq V = U1 * I;                             // V = U1 * I
+    alt_bn128_Fq X3 = r.squared() - J - (V+V);           // X3 = r^2 - J - 2 * V
+    alt_bn128_Fq S1_J = S1 * J;
+    alt_bn128_Fq Y3 = r * (V-X3) - (S1_J+S1_J);          // Y3 = r * (V-X3)-2 S1 J
+    alt_bn128_Fq Z3 = ((this->Z+other.Z).squared()-Z1Z1-Z2Z2) * H; // Z3 = ((Z1+Z2)^2-Z1Z1-Z2Z2) * H
+
+    return alt_bn128_G1(X3, Y3, Z3);
+}
+
+alt_bn128_G1 alt_bn128_G1::operator-() const
+{
+    return alt_bn128_G1(this->X, -(this->Y), this->Z);
+}
+
+
+alt_bn128_G1 alt_bn128_G1::operator-(const alt_bn128_G1 &other) const
+{
+    return (*this) + (-other);
+}
+
+alt_bn128_G1 alt_bn128_G1::add(const alt_bn128_G1 &other) const
+{
+    // handle special cases having to do with O
+    if (this->is_zero())
+    {
+        return other;
+    }
+
+    if (other.is_zero())
+    {
+        return *this;
+    }
+
+    // no need to handle points of order 2,4
+    // (they cannot exist in a prime-order subgroup)
+
+    // handle double case
+    if (this->operator==(other))
+    {
+        return this->dbl();
+    }
+
+#ifdef PROFILE_OP_COUNTS
+    this->add_cnt++;
+#endif
+    // NOTE: does not handle O and pts of order 2,4
+    // http://www.hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-0.html#addition-add-2007-bl
+
+    alt_bn128_Fq Z1Z1 = (this->Z).squared();             // Z1Z1 = Z1^2
+    alt_bn128_Fq Z2Z2 = (other.Z).squared();             // Z2Z2 = Z2^2
+    alt_bn128_Fq U1 = (this->X) * Z2Z2;                  // U1 = X1 * Z2Z2
+    alt_bn128_Fq U2 = (other.X) * Z1Z1;                  // U2 = X2 * Z1Z1
+    alt_bn128_Fq S1 = (this->Y) * (other.Z) * Z2Z2;      // S1 = Y1 * Z2 * Z2Z2
+    alt_bn128_Fq S2 = (other.Y) * (this->Z) * Z1Z1;      // S2 = Y2 * Z1 * Z1Z1
+    alt_bn128_Fq H = U2 - U1;                            // H = U2-U1
+    alt_bn128_Fq S2_minus_S1 = S2-S1;
+    alt_bn128_Fq I = (H+H).squared();                    // I = (2 * H)^2
+    alt_bn128_Fq J = H * I;                              // J = H * I
+    alt_bn128_Fq r = S2_minus_S1 + S2_minus_S1;          // r = 2 * (S2-S1)
+    alt_bn128_Fq V = U1 * I;                             // V = U1 * I
+    alt_bn128_Fq X3 = r.squared() - J - (V+V);           // X3 = r^2 - J - 2 * V
+    alt_bn128_Fq S1_J = S1 * J;
+    alt_bn128_Fq Y3 = r * (V-X3) - (S1_J+S1_J);          // Y3 = r * (V-X3)-2 S1 J
+    alt_bn128_Fq Z3 = ((this->Z+other.Z).squared()-Z1Z1-Z2Z2) * H; // Z3 = ((Z1+Z2)^2-Z1Z1-Z2Z2) * H
+
+    return alt_bn128_G1(X3, Y3, Z3);
+}
+
+alt_bn128_G1 alt_bn128_G1::mixed_add(const alt_bn128_G1 &other) const
+{
+#ifdef DEBUG
+    assert(other.is_special());
+#endif
+
+    // handle special cases having to do with O
+    if (this->is_zero())
+    {
+        return other;
+    }
+
+    if (other.is_zero())
+    {
+        return *this;
+    }
+
+    // no need to handle points of order 2,4
+    // (they cannot exist in a prime-order subgroup)
+
+    // check for doubling case
+
+    // using Jacobian coordinates so:
+    // (X1:Y1:Z1) = (X2:Y2:Z2)
+    // iff
+    // X1/Z1^2 == X2/Z2^2 and Y1/Z1^3 == Y2/Z2^3
+    // iff
+    // X1 * Z2^2 == X2 * Z1^2 and Y1 * Z2^3 == Y2 * Z1^3
+
+    // we know that Z2 = 1
+
+    const alt_bn128_Fq Z1Z1 = (this->Z).squared();
+
+    const alt_bn128_Fq &U1 = this->X;
+    const alt_bn128_Fq U2 = other.X * Z1Z1;
+
+    const alt_bn128_Fq Z1_cubed = (this->Z) * Z1Z1;
+
+    const alt_bn128_Fq &S1 = (this->Y);                // S1 = Y1 * Z2 * Z2Z2
+    const alt_bn128_Fq S2 = (other.Y) * Z1_cubed;      // S2 = Y2 * Z1 * Z1Z1
+
+    if (U1 == U2 && S1 == S2)
+    {
+        // dbl case; nothing of above can be reused
+        return this->dbl();
+    }
+
+#ifdef PROFILE_OP_COUNTS
+    this->add_cnt++;
+#endif
+
+    // NOTE: does not handle O and pts of order 2,4
+    // http://www.hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-0.html#addition-madd-2007-bl
+    alt_bn128_Fq H = U2-(this->X);                         // H = U2-X1
+    alt_bn128_Fq HH = H.squared() ;                        // HH = H&2
+    alt_bn128_Fq I = HH+HH;                                // I = 4*HH
+    I = I + I;
+    alt_bn128_Fq J = H*I;                                  // J = H*I
+    alt_bn128_Fq r = S2-(this->Y);                         // r = 2*(S2-Y1)
+    r = r + r;
+    alt_bn128_Fq V = (this->X) * I ;                       // V = X1*I
+    alt_bn128_Fq X3 = r.squared()-J-V-V;                   // X3 = r^2-J-2*V
+    alt_bn128_Fq Y3 = (this->Y)*J;                         // Y3 = r*(V-X3)-2*Y1*J
+    Y3 = r*(V-X3) - Y3 - Y3;
+    alt_bn128_Fq Z3 = ((this->Z)+H).squared() - Z1Z1 - HH; // Z3 = (Z1+H)^2-Z1Z1-HH
+
+    return alt_bn128_G1(X3, Y3, Z3);
+}
+
+alt_bn128_G1 alt_bn128_G1::dbl() const
+{
+#ifdef PROFILE_OP_COUNTS
+    this->dbl_cnt++;
+#endif
+    // handle point at infinity
+    if (this->is_zero())
+    {
+        return (*this);
+    }
+
+    // no need to handle points of order 2,4
+    // (they cannot exist in a prime-order subgroup)
+
+    // NOTE: does not handle O and pts of order 2,4
+    // http://www.hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-0.html#doubling-dbl-2009-l
+
+    alt_bn128_Fq A = (this->X).squared();         // A = X1^2
+    alt_bn128_Fq B = (this->Y).squared();        // B = Y1^2
+    alt_bn128_Fq C = B.squared();                // C = B^2
+    alt_bn128_Fq D = (this->X + B).squared() - A - C;
+    D = D+D;                        // D = 2 * ((X1 + B)^2 - A - C)
+    alt_bn128_Fq E = A + A + A;                  // E = 3 * A
+    alt_bn128_Fq F = E.squared();                // F = E^2
+    alt_bn128_Fq X3 = F - (D+D);                 // X3 = F - 2 D
+    alt_bn128_Fq eightC = C+C;
+    eightC = eightC + eightC;
+    eightC = eightC + eightC;
+    alt_bn128_Fq Y3 = E * (D - X3) - eightC;     // Y3 = E * (D - X3) - 8 * C
+    alt_bn128_Fq Y1Z1 = (this->Y)*(this->Z);
+    alt_bn128_Fq Z3 = Y1Z1 + Y1Z1;               // Z3 = 2 * Y1 * Z1
+
+    return alt_bn128_G1(X3, Y3, Z3);
+}
+
+bool alt_bn128_G1::is_well_formed() const
+{
+    if (this->is_zero())
+    {
+        return true;
+    }
+    else
+    {
+        /*
+          y^2 = x^3 + b
+
+          We are using Jacobian coordinates, so equation we need to check is actually
+
+          (y/z^3)^2 = (x/z^2)^3 + b
+          y^2 / z^6 = x^3 / z^6 + b
+          y^2 = x^3 + b z^6
+        */
+        alt_bn128_Fq X2 = this->X.squared();
+        alt_bn128_Fq Y2 = this->Y.squared();
+        alt_bn128_Fq Z2 = this->Z.squared();
+
+        alt_bn128_Fq X3 = this->X * X2;
+        alt_bn128_Fq Z3 = this->Z * Z2;
+        alt_bn128_Fq Z6 = Z3.squared();
+
+        return (Y2 == X3 + alt_bn128_coeff_b * Z6);
+    }
+}
+
+alt_bn128_G1 alt_bn128_G1::zero()
+{
+    return G1_zero;
+}
+
+alt_bn128_G1 alt_bn128_G1::one()
+{
+    return G1_one;
+}
+
+
+
+void alt_bn128_G1::batch_to_special_all_non_zeros(std::vector<alt_bn128_G1> &vec)
+{
+    std::vector<alt_bn128_Fq> Z_vec;
+    Z_vec.reserve(vec.size());
+
+    for (auto &el: vec)
+    {
+        Z_vec.emplace_back(el.Z);
+    }
+    batch_invert<alt_bn128_Fq>(Z_vec);
+
+    const alt_bn128_Fq one = alt_bn128_Fq::one();
+
+    for (size_t i = 0; i < vec.size(); ++i)
+    {
+        alt_bn128_Fq Z2 = Z_vec[i].squared();
+        alt_bn128_Fq Z3 = Z_vec[i] * Z2;
+
+        vec[i].X = vec[i].X * Z2;
+        vec[i].Y = vec[i].Y * Z3;
+        vec[i].Z = one;
+    }
+}
+
+} // libff
---
+++ trusted_libff/libff/libff/algebra/curves/alt_bn128/alt_bn128_init.cpp
@@ -0,0 +1,159 @@
+/** @file
+ *****************************************************************************
+ * @author     This file is part of libff, developed by SCIPR Lab
+ *             and contributors (see AUTHORS).
+ * @copyright  MIT license (see LICENSE file)
+ *****************************************************************************/
+
+#include <libff/algebra/curves/alt_bn128/alt_bn128_g1.hpp>
+#include <libff/algebra/curves/alt_bn128/alt_bn128_init.hpp>
+
+namespace libff {
+
+bigint<alt_bn128_r_limbs> alt_bn128_modulus_r;
+bigint<alt_bn128_q_limbs> alt_bn128_modulus_q;
+
+alt_bn128_Fq alt_bn128_coeff_b;
+alt_bn128_Fq alt_bn128_twist_mul_by_b_c0;
+alt_bn128_Fq alt_bn128_twist_mul_by_b_c1;
+
+bigint<alt_bn128_q_limbs> alt_bn128_ate_loop_count;
+bool alt_bn128_ate_is_loop_count_neg;
+bigint<12*alt_bn128_q_limbs> alt_bn128_final_exponent;
+bigint<alt_bn128_q_limbs> alt_bn128_final_exponent_z;
+bool alt_bn128_final_exponent_is_z_neg;
+
+void init_alt_bn128_params()
+{
+    typedef bigint<alt_bn128_r_limbs> bigint_r;
+    typedef bigint<alt_bn128_q_limbs> bigint_q;
+
+    assert(sizeof(mp_limb_t) == 8 || sizeof(mp_limb_t) == 4); // Montgomery assumes this
+
+    /* parameters for scalar field Fr */
+
+    alt_bn128_modulus_r = bigint_r("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+    assert(alt_bn128_Fr::modulus_is_valid());
+    if (sizeof(mp_limb_t) == 8)
+    {
+        alt_bn128_Fr::Rsquared = bigint_r("944936681149208446651664254269745548490766851729442924617792859073125903783");
+        alt_bn128_Fr::Rcubed = bigint_r("5866548545943845227489894872040244720403868105578784105281690076696998248512");
+        alt_bn128_Fr::inv = 0xc2e1f593efffffff;
+    }
+    if (sizeof(mp_limb_t) == 4)
+    {
+        alt_bn128_Fr::Rsquared = bigint_r("944936681149208446651664254269745548490766851729442924617792859073125903783");
+        alt_bn128_Fr::Rcubed = bigint_r("5866548545943845227489894872040244720403868105578784105281690076696998248512");
+        alt_bn128_Fr::inv = 0xefffffff;
+    }
+    alt_bn128_Fr::num_bits = 254;
+    alt_bn128_Fr::euler = bigint_r("10944121435919637611123202872628637544274182200208017171849102093287904247808");
+    alt_bn128_Fr::s = 28;
+    alt_bn128_Fr::t = bigint_r("81540058820840996586704275553141814055101440848469862132140264610111");
+    alt_bn128_Fr::t_minus_1_over_2 = bigint_r("40770029410420498293352137776570907027550720424234931066070132305055");
+    alt_bn128_Fr::multiplicative_generator = alt_bn128_Fr("5");
+    alt_bn128_Fr::root_of_unity = alt_bn128_Fr("19103219067921713944291392827692070036145651957329286315305642004821462161904");
+    alt_bn128_Fr::nqr = alt_bn128_Fr("5");
+    alt_bn128_Fr::nqr_to_t = alt_bn128_Fr("19103219067921713944291392827692070036145651957329286315305642004821462161904");
+
+    /* parameters for base field Fq */
+
+    alt_bn128_modulus_q = bigint_q("21888242871839275222246405745257275088696311157297823662689037894645226208583");
+    assert(alt_bn128_Fq::modulus_is_valid());
+    if (sizeof(mp_limb_t) == 8)
+    {
+        alt_bn128_Fq::Rsquared = bigint_q("3096616502983703923843567936837374451735540968419076528771170197431451843209");
+        alt_bn128_Fq::Rcubed = bigint_q("14921786541159648185948152738563080959093619838510245177710943249661917737183");
+        alt_bn128_Fq::inv = 0x87d20782e4866389;
+    }
+    if (sizeof(mp_limb_t) == 4)
+    {
+        alt_bn128_Fq::Rsquared = bigint_q("3096616502983703923843567936837374451735540968419076528771170197431451843209");
+        alt_bn128_Fq::Rcubed = bigint_q("14921786541159648185948152738563080959093619838510245177710943249661917737183");
+        alt_bn128_Fq::inv = 0xe4866389;
+    }
+    alt_bn128_Fq::num_bits = 254;
+    alt_bn128_Fq::euler = bigint_q("10944121435919637611123202872628637544348155578648911831344518947322613104291");
+    alt_bn128_Fq::s = 1;
+    alt_bn128_Fq::t = bigint_q("10944121435919637611123202872628637544348155578648911831344518947322613104291");
+    alt_bn128_Fq::t_minus_1_over_2 = bigint_q("5472060717959818805561601436314318772174077789324455915672259473661306552145");
+    alt_bn128_Fq::multiplicative_generator = alt_bn128_Fq("3");
+    alt_bn128_Fq::root_of_unity = alt_bn128_Fq("21888242871839275222246405745257275088696311157297823662689037894645226208582");
+    alt_bn128_Fq::nqr = alt_bn128_Fq("3");
+    alt_bn128_Fq::nqr_to_t = alt_bn128_Fq("21888242871839275222246405745257275088696311157297823662689037894645226208582");
+
+
+    /* choice of short Weierstrass curve and its twist */
+
+    alt_bn128_coeff_b = alt_bn128_Fq("3");
+
+    /* choice of group G1 */
+    alt_bn128_G1::G1_zero = alt_bn128_G1(alt_bn128_Fq::zero(),
+                                     alt_bn128_Fq::one(),
+                                     alt_bn128_Fq::zero());
+    alt_bn128_G1::G1_one = alt_bn128_G1(alt_bn128_Fq("1"),
+                                    alt_bn128_Fq("2"),
+                                    alt_bn128_Fq::one());
+    alt_bn128_G1::wnaf_window_table.resize(0);
+    alt_bn128_G1::wnaf_window_table.push_back(11);
+    alt_bn128_G1::wnaf_window_table.push_back(24);
+    alt_bn128_G1::wnaf_window_table.push_back(60);
+    alt_bn128_G1::wnaf_window_table.push_back(127);
+
+    alt_bn128_G1::fixed_base_exp_window_table.resize(0);
+    // window 1 is unbeaten in [-inf, 4.99]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(1);
+    // window 2 is unbeaten in [4.99, 10.99]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(5);
+    // window 3 is unbeaten in [10.99, 32.29]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(11);
+    // window 4 is unbeaten in [32.29, 55.23]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(32);
+    // window 5 is unbeaten in [55.23, 162.03]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(55);
+    // window 6 is unbeaten in [162.03, 360.15]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(162);
+    // window 7 is unbeaten in [360.15, 815.44]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(360);
+    // window 8 is unbeaten in [815.44, 2373.07]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(815);
+    // window 9 is unbeaten in [2373.07, 6977.75]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(2373);
+    // window 10 is unbeaten in [6977.75, 7122.23]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(6978);
+    // window 11 is unbeaten in [7122.23, 57818.46]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(7122);
+    // window 12 is never the best
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(0);
+    // window 13 is unbeaten in [57818.46, 169679.14]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(57818);
+    // window 14 is never the best
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(0);
+    // window 15 is unbeaten in [169679.14, 439758.91]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(169679);
+    // window 16 is unbeaten in [439758.91, 936073.41]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(439759);
+    // window 17 is unbeaten in [936073.41, 4666554.74]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(936073);
+    // window 18 is never the best
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(0);
+    // window 19 is unbeaten in [4666554.74, 7580404.42]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(4666555);
+    // window 20 is unbeaten in [7580404.42, 34552892.20]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(7580404);
+    // window 21 is never the best
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(0);
+    // window 22 is unbeaten in [34552892.20, inf]
+    alt_bn128_G1::fixed_base_exp_window_table.push_back(34552892);
+
+
+    /* pairing parameters */
+
+    alt_bn128_ate_loop_count = bigint_q("29793968203157093288");
+    alt_bn128_ate_is_loop_count_neg = false;
+    alt_bn128_final_exponent = bigint<12*alt_bn128_q_limbs>("552484233613224096312617126783173147097382103762957654188882734314196910839907541213974502761540629817009608548654680343627701153829446747810907373256841551006201639677726139946029199968412598804882391702273019083653272047566316584365559776493027495458238373902875937659943504873220554161550525926302303331747463515644711876653177129578303191095900909191624817826566688241804408081892785725967931714097716709526092261278071952560171111444072049229123565057483750161460024353346284167282452756217662335528813519139808291170539072125381230815729071544861602750936964829313608137325426383735122175229541155376346436093930287402089517426973178917569713384748081827255472576937471496195752727188261435633271238710131736096299798168852925540549342330775279877006784354801422249722573783561685179618816480037695005515426162362431072245638324744480");
+    alt_bn128_final_exponent_z = bigint_q("4965661367192848881");
+    alt_bn128_final_exponent_is_z_neg = false;
+
+}
+} // libff
