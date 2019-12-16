fb2fbbb7a9670882d885fff7885d0a80fb0455d1
Stan Kladko <13399135+kladkogex@users.noreply.github.com>
2019-12-16 13:00:14 +0200

    Update install_packages.sh

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,2 +1,2 @@
-dpkg -i *.deb
+dpkg -i libsgx-enclave-common_2.5.101.50123-bionic1_amd64.deb
 apt install  cmake flex bison  libprocps-dev ccache autoconf texinfo libssl-dev libboost-all-dev libjsonrpccpp-dev libjsonrpccpp-tools
