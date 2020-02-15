631caea7fe0aa0b64d36c7d30a081bfbf46d431e
kladko <13399135+kladkogex@users.noreply.github.com>
2020-02-15 20:22:48 +0200

    SKALE-2077 Fixed Naming

--- docker/start.sh
+++ docker/start.sh
@@ -8,5 +8,28 @@ jhid -d
 pid=$!
 
 sleep 2
-cd /usr/src/sdk; ./sgxwallet $1 $2 $3 $4
+cd /usr/src/sdk;
+
+if [ "$1" == "-t" ]; then
+  ./testw [bls-key-encrypt]
+  ./testw [bls-key-encrypt-decrypt]
+  ./testw [dkg-gen]
+  ./testw [dkg-pub_shares]
+  ./testw [dkg-encr_sshares]
+  ./testw [dkg-verify]
+  ./testw [ecdsa_test]
+  ./testw [test_test]
+  ./testw [get_pub_ecdsa_key_test]
+  ./testw [bls_dkg]
+  ./testw [api_test]
+  ./testw [getServerStatus_test]
+  ./testw [many_threads_test]
+  ./testw [ecdsa_api_test]
+  ./testw [dkg_api_test]
+  ./testw [is_poly_test]
+  ./testw [aes_dkg]
+  ./testw [AES-encrypt-decrypt]
+else
+   ./sgxwallet $1 $2 $3 $4
+fi
 
