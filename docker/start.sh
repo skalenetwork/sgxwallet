a3c8931c641883a0b679cb00e882018c7134a447
kladko <13399135+kladkogex@users.noreply.github.com>
2020-03-26 22:24:57 +0200

    SKALE-2345 fix hw

--- docker/start.sh
+++ docker/start.sh
@@ -9,37 +9,38 @@ cd /usr/src/sdk;
 
 if [ -f "/var/hwmode" ]
 then
-  echo "Running in SGX hardware mode"
-  jhid -d
-  /opt/intel/sgxpsw/aesm/aesm_service &
-  pid=$!
-  sleep 2
+echo "Running in SGX hardware mode"
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/intel/sgxpsw/aesm/
+jhid -d
+/opt/intel/sgxpsw/aesm/aesm_service &
+pid=$!
+sleep 2
 else
-  echo "Running in SGX simulation mode"
+echo "Running in SGX simulation mode"
 fi
 
 
 if [ "$1" = -t ]; then
-  echo "Test run requested"
-#./testw [bls-key-encrypt]
-#./testw [bls-key-encrypt-decrypt]
-#./testw [dkg-gen]
-#./testw [dkg-pub_shares]
-#./testw [dkg-verify]
+echo "Test run requested"
+./testw [bls-key-encrypt]
+./testw [bls-key-encrypt-decrypt]
+./testw [dkg-gen]
+./testw [dkg-pub_shares]
+./testw [dkg-verify]
 ./testw [ecdsa_test]
-#./testw [test_test]
-#./testw [get_pub_ecdsa_key_test]
-#./testw [bls_dkg]
-#./testw [api_test]
-#./testw [getServerStatus_test]
-#./testw [dkg_api_test]
-#./testw [is_poly_test]
-#./testw [AES-encrypt-decrypt]
-#./testw [ecdsa_api_test]
-#./testw [dkg-encr_sshares]
-#./testw [bls_sign]
-#/testw [many_threads_test]
-#./testw [aes_dkg]
+./testw [test_test]
+./testw [get_pub_ecdsa_key_test]
+./testw [bls_dkg]
+./testw [api_test]
+./testw [getServerStatus_test]
+./testw [dkg_api_test]
+./testw [is_poly_test]
+./testw [AES-encrypt-decrypt]
+./testw [ecdsa_api_test]
+./testw [dkg-encr_sshares]
+./testw [bls_sign]
+/testw [many_threads_test]
+./testw [aes_dkg]
 else
    ./sgxwallet $1 $2 $3 $4
 fi
