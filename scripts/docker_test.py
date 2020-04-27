ff3e1080ac18479f8f78fafde784ee8b92657d31
kladko <13399135+kladkogex@users.noreply.github.com>
2020-04-27 23:51:06 +0300

    SKALE-2502

--- scripts/docker_test.py
+++ scripts/docker_test.py
@@ -50,7 +50,7 @@ FULL_IMAGE_NAME = "skalenetwork/" + IMAGE_NAME +":" + TAG_POSTFIX;
 print("Running tests for branch " + BRANCH);
 
 dockerRun = subprocess.run(["docker", "run", "-v", topDir + "/sgx_data:/usr/src/sdk/sgx_data","-t",
-                            "--device", "/dev/urandom:/dev/random", "--name", "sgxwallet", "--network=host", "skalenetwork/" + IMAGE_NAME +":" + TAG_POSTFIX, "-t"])
+                            "-v", "/dev/urandom:/dev/random", "--name", "sgxwallet", "--network=host", "skalenetwork/" + IMAGE_NAME +":" + TAG_POSTFIX, "-t"])
 
 print(dockerRun.stdout)
 print(dockerRun.stderr)
