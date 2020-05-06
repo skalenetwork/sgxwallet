0ed2e27c75a90c9a117c14fb028d91aeddc2e2e1
kladko <13399135+kladkogex@users.noreply.github.com>
2020-05-06 18:37:44 +0300

    Fixed docs

--- DKGCrypto.h
+++ DKGCrypto.h
@@ -27,23 +27,25 @@
 #include <string>
 #include <vector>
 
-std::string gen_dkg_poly( int _t);
+using namespace std;
 
-std::vector <std::vector<std::string>> get_verif_vect(const char* encryptedPolyHex, int t, int n);
+string gen_dkg_poly( int _t);
 
-std::vector<std::string> splitString(const char* coeffs, const char symbol);
+vector <vector<string>> get_verif_vect(const char* encryptedPolyHex, int t, int n);
 
-std::string trustedGetSecretShares(const std::string& _polyName, const char* _encryptedPolyHex, const std::vector<std::string>& _publicKeys, int _t, int _n);
+vector<string> splitString(const char* coeffs, const char symbol);
+
+string trustedGetSecretShares(const string& _polyName, const char* _encryptedPolyHex, const vector<string>& _publicKeys, int _t, int _n);
 
 bool verifyShares(const char* publicShares, const char* encr_sshare, const char * encryptedKeyHex, int t, int n, int ind);
 
-std::string decryptDHKey(const std::string& polyName, int ind);
+string decryptDHKey(const string& polyName, int ind);
 
-bool CreateBLSShare( const std::string& blsKeyName, const char * s_shares, const char * encryptedKeyHex);
+bool CreateBLSShare( const string& blsKeyName, const char * s_shares, const char * encryptedKeyHex);
 
-std::vector<std::string> GetBLSPubKey(const char * encryptedKeyHex);
+vector<string> GetBLSPubKey(const char * encryptedKeyHex);
 
-std::vector<std::string> mult_G2(const std::string& x);
+vector<string> mult_G2(const string& x);
 
 
 
