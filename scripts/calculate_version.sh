e1fced54d7dff97f3980de3bf64cc644eda1c5f5
Oleh Nikolaiev <oleg@skalelabs.com>
2020-05-18 16:33:43 +0300

    SKALE-2202 fix codacy warnings

--- scripts/calculate_version.sh
+++ scripts/calculate_version.sh
@@ -4,19 +4,19 @@ BRANCH=$1
 VERSION=$2
 CONTAINER=$3
 
-if [ -z $BRANCH ]
+if [ -z "$BRANCH" ]
 then
       echo "A branch is not set."
       exit 1
 fi
 
-if [ -z $VERSION ]
+if [ -z "$VERSION" ]
 then
       echo "The base version is not set."
       exit 1
 fi
 
-if [ -z $CONTAINER ]
+if [ -z "$CONTAINER" ]
 then
       echo "The base container is not set."
       exit 1
@@ -24,17 +24,17 @@ fi
 
 git fetch --tags
 
-if [ $BRANCH = "master" ]
+if [ "$BRANCH" = "master" ]
 then
-    echo $VERSION
+    echo "$VERSION"
     exit 0
 fi
 
 LABEL="develop"
-if [ $BRANCH = "stable" ]
+if [ "$BRANCH" = "stable" ]
 then
     LABEL="stable"
-elif [ $BRANCH = "beta" ]
+elif [ "$BRANCH" = "beta" ]
 then
     LABEL="beta"
 fi
@@ -42,9 +42,9 @@ fi
 for (( VERSION_NUMBER=0; ; VERSION_NUMBER++ ))
 do
     RESULT_VERSION="$CONTAINER:$VERSION-$LABEL.$VERSION_NUMBER"
-    if ! [ $(git tag -l ?$RESULT_VERSION) ]
+    if ! [ "$(git tag -l ?"$RESULT_VERSION")" ]
     then
-        echo $RESULT_VERSION
+        echo "$RESULT_VERSION"
         break
     fi
 done
