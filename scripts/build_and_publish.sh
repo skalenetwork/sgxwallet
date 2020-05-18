b19f18e28bb15ec66778a16cca71201425fc77ee
Oleh Nikolaiev <oleg@skalelabs.com>
2020-05-18 16:45:41 +0300

    SKALE-2202 fix codacy warnings

--- scripts/build_and_publish.sh
+++ scripts/build_and_publish.sh
@@ -16,8 +16,8 @@ LATEST_IMAGE_NAME=$REPO_NAME:$CONTAINER:$BRANCH-latest
 # Build image
 
 echo "Building $IMAGE_NAME..."
-docker build -t $IMAGE_NAME --file $DOCKERFILE || exit $?
-docker tag $IMAGE_NAME $LATEST_IMAGE_NAME
+docker build -t "$IMAGE_NAME" --file "$DOCKERFILE" || exit $?
+docker tag "$IMAGE_NAME" "$LATEST_IMAGE_NAME"
 
 echo "========================================================================================="
 echo "Built $IMAGE_NAME"
@@ -27,7 +27,7 @@ echo "Built $IMAGE_NAME"
 : "${DOCKER_USERNAME?Need to set DOCKER_USERNAME}"
 : "${DOCKER_PASSWORD?Need to set DOCKER_PASSWORD}"
 
-echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-docker push $IMAGE_NAME || exit $?
-docker push $LATEST_IMAGE_NAME || exit $?
+docker push "$IMAGE_NAME" || exit $?
+docker push "$LATEST_IMAGE_NAME" || exit $?
