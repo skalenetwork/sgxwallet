c7cc387bc8e90eb7f597b7ace809b05bca3a7533
Oleh Nikolaiev <oleg@skalelabs.com>
2020-05-19 16:16:36 +0300

    SKALE-2202 build and test for sim mode

--- scripts/publish_image.sh
+++ scripts/publish_image.sh
@@ -12,19 +12,6 @@ REPO_NAME=skalenetwork/$NAME
 IMAGE_NAME=$REPO_NAME:$VERSION
 LATEST_IMAGE_NAME=$REPO_NAME:$BRANCH-latest
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-# Build image
-
-echo "Building $IMAGE_NAME..."
-docker build -f "${DIR}"/../"${DOCKERFILE}" -t "${IMAGE_NAME}" . || exit $?
-docker tag "${IMAGE_NAME}" "${LATEST_IMAGE_NAME}"
-
-echo "========================================================================================="
-echo "Built $IMAGE_NAME"
-
-# Publish image
-
 : "${DOCKER_USERNAME?Need to set DOCKER_USERNAME}"
 : "${DOCKER_PASSWORD?Need to set DOCKER_PASSWORD}"
 
