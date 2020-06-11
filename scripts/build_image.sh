61a5934608dac021304d5b0cb3dc318f7f237a11
Oleh Nikolaiev <oleg@skalelabs.com>
2020-06-11 09:24:41 +0300

    add latest tag

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -8,10 +8,8 @@ CONTAINER_NAME=$2
 : "${VERSION?Need to set VERSION}"
 : "${BRANCH?Need to set BRANCH}"
 
-NAME=sgx
-REPO_NAME=skalenetwork/$NAME
-IMAGE_NAME=$REPO_NAME.$CONTAINER_NAME:$VERSION
-LATEST_IMAGE_NAME=$REPO_NAME:$BRANCH-latest
+REPO_NAME=skalenetwork/$CONTAINER_NAME
+IMAGE_NAME=$REPO_NAME:$VERSION
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -19,7 +17,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "Building $IMAGE_NAME..."
 docker build -f "${DIR}"/../"${DOCKERFILE}" -t "${IMAGE_NAME}" . || exit $?
-docker tag "${IMAGE_NAME}" "${LATEST_IMAGE_NAME}"
+
+if [ "${BRANCH}" = "stable" ];
+then
+	LATEST_IMAGE_NAME=$REPO_NAME:latest
+	docker tag "${IMAGE_NAME}" "${LATEST_IMAGE_NAME}"
+else
+	LATEST_IMAGE_NAME=$REPO_NAME:$BRANCH-latest
+	docker tag "${IMAGE_NAME}" "${LATEST_IMAGE_NAME}"
+fi
 
 echo "========================================================================================="
 echo "Built $IMAGE_NAME"
