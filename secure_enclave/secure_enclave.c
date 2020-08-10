d44dae07a765198e1997edd02954db46d6ae7be1
kladko <13399135+kladkogex@users.noreply.github.com>
2020-08-10 17:29:15 +0300

    Revert "SKALE-3067-cleanup-sgx"
    This reverts commit 4ec645fc38bc3dd475111ff435a352a084edf479.

--- secure_enclave/secure_enclave.c
+++ secure_enclave/secure_enclave.c
@@ -981,7 +981,7 @@ void trustedGetBlsPubKey(int *errStatus, char *errString, uint8_t *encryptedPriv
     *errStatus = UNKNOWN_ERROR;
 
 
-    SAFE_CHAR_BUF(skey_hex,ECDSA_SKEY_LEN);
+    char skey_hex[ECDSA_SKEY_LEN];
 
     uint32_t len = key_len;
 
@@ -1015,8 +1015,7 @@ void trustedGenerateSEK(int *errStatus, char *errString,
     *errString = 0;
     *errStatus = UNKNOWN_ERROR;
 
-    SAFE_CHAR_BUF(SEK_raw,SGX_AESGCM_KEY_SIZE);
-
+    uint8_t SEK_raw[SGX_AESGCM_KEY_SIZE];
     sgx_read_rand(SEK_raw, SGX_AESGCM_KEY_SIZE);
 
     uint32_t hex_aes_key_length = SGX_AESGCM_KEY_SIZE * 2;
@@ -1051,7 +1050,7 @@ void trustedSetSEK(int *errStatus, char *errString, uint8_t *encrypted_SEK, uint
 
 
 
-    SAFE_CHAR_BUF(aes_key_hex,SGX_AESGCM_KEY_SIZE * 2);
+    uint8_t aes_key_hex[SGX_AESGCM_KEY_SIZE * 2];
     memset(aes_key_hex, 0, SGX_AESGCM_KEY_SIZE * 2);
 
     sgx_status_t status = sgx_unseal_data(
