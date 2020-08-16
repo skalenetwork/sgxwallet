020cf90dff24723103c2a27caa0b4f0ce866fe9e
kladko <13399135+kladkogex@users.noreply.github.com>
2020-08-16 18:56:25 +0300

    Cleaning signing

--- scripts/sign_enclave.bash
+++ scripts/sign_enclave.bash
@@ -2,6 +2,8 @@
 set -x
 set -e
 
+rm -f skale_sgx_enclave_hash1.hex skale_sgx_enclave_signature1.hex ../secure_enclave/secure_enclave_signed.so skale_sgx_enclave_metadata_info1.txt 
+
 /opt/intel/sgxsdk/bin/x64/sgx_sign gendata -enclave ../secure_enclave/secure_enclave.so -config ../secure_enclave/secure_enclave.config.xml -out skale_sgx_enclave_hash1.hex
 
 openssl dgst -sha256 -out skale_sgx_enclave_signature1.hex -sign skale_sgx_private_key1.pem -keyform PEM skale_sgx_enclave_hash1.hex
