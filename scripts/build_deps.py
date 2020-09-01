8d3c685421f06f9ea8bf2b55e6d88affcecac11d
kladko <13399135+kladkogex@users.noreply.github.com>
2020-09-01 14:58:20 +0300

    Typo

--- scripts/build_deps.py
+++ scripts/build_deps.py
@@ -102,7 +102,7 @@ assert subprocess.call(["make", "install"]) == 0
 assert subprocess.call(["make", "clean"]) == 0
 
 os.chdir(topDir)
-assert subprocess.call(["cp", "third_party/gmp/sgx_tgmp.h.fixed", TGMP_BUILD_DIR + "/include/sgx_tgmp.h"]) == -2
+assert subprocess.call(["cp", "third_party/gmp/sgx_tgmp.h.fixed", TGMP_BUILD_DIR + "/include/sgx_tgmp.h"]) == 0 
 
 os.chdir(topDir)
 print("Build successfull.")
