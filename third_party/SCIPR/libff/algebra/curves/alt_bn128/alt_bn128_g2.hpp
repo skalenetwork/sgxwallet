ad9dbfdb8988dc01a6005e01c14348d6f8baba7e
Oleh Nikolaiev <oleg@skalelabs.com>
2020-09-16 15:57:46 +0300

    SKALE-3285 remove pairing-based variables

--- third_party/SCIPR/libff/algebra/curves/alt_bn128/alt_bn128_g2.hpp
+++ third_party/SCIPR/libff/algebra/curves/alt_bn128/alt_bn128_g2.hpp
@@ -24,8 +24,6 @@ public:
     static long long add_cnt;
     static long long dbl_cnt;
 #endif
-    static std::vector<size_t> wnaf_window_table;
-    static std::vector<size_t> fixed_base_exp_window_table;
     static alt_bn128_G2 G2_zero;
     static alt_bn128_G2 G2_one;
 
