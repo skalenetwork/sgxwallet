601ceced4a71cebb845d5452767d48e0f1c34d4e
Stan Kladko <13399135+kladkogex@users.noreply.github.com>
2021-01-08 16:19:06 +0200

    :xy xx

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sudo dpkg -i *.deb
 sudo apt install -y build-essential make gcc g++ yasm  python libprotobuf10 flex bison automake
 sudo apt install -y ccache cmake ccache autoconf texinfo libgcrypt20-dev libgnutls28-dev libtool pkg-config
