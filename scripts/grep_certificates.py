402c7ee440bfd04d1ef62118addc22a251c17ed9
Oleh <oleg@skalelabs.com>
2022-08-16 13:32:24 +0300

    add doc and script to check certificates

---
+++ scripts/grep_certificates.py
@@ -0,0 +1,37 @@
+import os
+import re
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("Wrong number of command line arguments: need exactly one")
+        exit(1)
+
+    path = sys.argv[1]
+    if not os.path.exists(path):
+        print("No such file or directory: ", path)
+        exit(2)
+    
+    certs_path = os.path.join(path, "cert_data", "new_certs")
+    if len(os.listdir(certs_path)) == 0:
+        print("Empty certificates directory. Nothing to review.")
+        return
+    
+    for entity in os.listdir(certs_path):
+        entity_path = os.path.join(certs_path, entity)
+        if not os.path.isfile(entity_path):
+            print("Not a regular file. Skipping.")
+            continue
+        _, extension = os.path.splitext(entity_path)
+        if extension != '.pem':
+            print("Not a ssl certificate file. Skipping.")
+            continue
+        with open(entity_path,"r") as file_one:
+            pattern = "Not Before"
+            for line in file_one:
+                if re.search(pattern, line):
+                    print(line)
+
+
+if __name__ == '__main__':
+    main()
