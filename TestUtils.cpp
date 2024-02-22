cd419fc3df51f793742f3e98f032bc42ff66853e
Oleh <oleg@skalelabs.com>
2024-02-22 15:36:01 +0000

    add tests

--- TestUtils.cpp
+++ TestUtils.cpp
@@ -813,7 +813,7 @@ void TestUtils::doDKGV2(StubClient &c, int n, int t,
   for (int i = 0; i < n; i++) {
     vector<string> pubKeyVect;
     for (uint8_t j = 0; j < 4; j++) {
-      pubKeyVect.push_back(allPubKeysJson["result"][i][j].asString());
+      pubKeyVect.push_back(allPubKeysJson["result"]["publicKeys"][i][j].asString());
     }
     BLSPublicKeyShare pubKey(make_shared<vector<string>>(pubKeyVect), t, n);
 
