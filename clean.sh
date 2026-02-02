a16a1de629ef62a19f98360f6e9ec21eadc6e719
Sidnei Teixeira <sidnei.teixeira@skalelabs.com>
2026-02-02 16:28:39 +0000

    #492 add full clean option to clean all dependencies

--- clean.sh
+++ clean.sh
@@ -1,8 +1,3 @@
-# clean all libBLS dependencies
-cd libBLS/deps
-./clean.sh
-cd ../..
-
 # clean all build files
 rm -rf *.o
 rm -rf .deps/*.Po
@@ -10,3 +5,8 @@ rm -rf secure_enclave/*.o
 rm -rf secure_enclave/.deps/*.Po
 rm -rf Makefile Makefile.in
 rm -rf secure_enclave/Makefile
+
+if [ "$1" = "--full" ]; then
+  ./libBLS/deps/clean.sh
+  git submodule foreach --recursive git clean -xfd
+fi
