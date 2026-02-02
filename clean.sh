79dcff3620919cc177153fdf94aa6e65d050dcc0
Sidnei Teixeira <sidnei.teixeira@skalelabs.com>
2026-02-02 19:21:08 +0000

    #492 make full clean stronger

--- clean.sh
+++ clean.sh
@@ -7,6 +7,6 @@ rm -rf Makefile Makefile.in
 rm -rf secure_enclave/Makefile
 
 if [ "$1" = "--full" ]; then
-  ./libBLS/deps/clean.sh
-  git submodule foreach --recursive git clean -xfd
+  git submodule update --init --recursive --force
+  git submodule foreach --recursive 'git reset --hard && git clean -xfd'
 fi
