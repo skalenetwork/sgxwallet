cfde05c22570eed64456878a5b9e47707fa20534
Sidnei Teixeira <sidnei.teixeira@skalelabs.com>
2026-02-05 12:15:46 +0000

    #492 update backward_compatibiltiy makefile; run backward tests in CI

--- docker/start.sh
+++ docker/start.sh
@@ -48,6 +48,8 @@ if [[ "$1" == "-t" ]]; then
 echo "Test run requested"
 sleep 5
 ./testw.py
+echo "Running backward compatibility tests"
+./tests/backward_compatibility/run.sh
 else
 /usr/src/sdk/check_firewall.py
 ./sgxwallet $1 $2 $3 $4 $5 $6
