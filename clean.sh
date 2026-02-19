2d7d1784d8e6ccdadea04ad04e3504a3bd86b781
Sidnei Teixeira <sidnei.teixeira@skalelabs.com>
2026-02-19 19:41:16 +0000

    #492 update clean script to remove all .m4 files

--- clean.sh
+++ clean.sh
@@ -6,6 +6,16 @@ rm -rf secure_enclave/.deps/*.Po
 rm -rf Makefile Makefile.in
 rm -rf secure_enclave/Makefile
 
+# Remove all libtool generated files
+# TODO - remove this
+rm -f ltmain.sh libtool
+rm -f m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4
+find . -name '.libs' -type d -prune -exec rm -rf {} +
+find . -name '*.la' -o -name '*.lo' | xargs -r rm -f
+
+rm ./*.m4
+
+# Remove all generated files in all submodules
 if [ "$1" = "--full" ]; then
   git submodule update --init --recursive --force
   git submodule foreach --recursive 'git reset --hard && git clean -xfd'
