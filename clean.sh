851a8bb63001e1c68226deac02f2ebf3f917ad1e
Sidnei Teixeira <sidnei.teixeira@skalelabs.com>
2026-02-19 19:43:46 +0000

    #492 remove unused libtoolize cleanup

--- clean.sh
+++ clean.sh
@@ -6,13 +6,6 @@ rm -rf secure_enclave/.deps/*.Po
 rm -rf Makefile Makefile.in
 rm -rf secure_enclave/Makefile
 
-# Remove all libtool generated files
-# TODO - remove this
-rm -f ltmain.sh libtool
-rm -f m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4
-find . -name '.libs' -type d -prune -exec rm -rf {} +
-find . -name '*.la' -o -name '*.lo' | xargs -r rm -f
-
 rm ./*.m4
 
 # Remove all generated files in all submodules
