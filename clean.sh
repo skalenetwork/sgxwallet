b95e7fb49b334b2f1743ee80089dd81cb0288835
Sidnei Teixeira <sidnei.teixeira@skalelabs.com>
2026-03-06 18:13:02 +0000

    #499 fix autoconfig warnings

--- clean.sh
+++ clean.sh
@@ -1,6 +1,13 @@
 # clean all build files
 rm -rf *.o
 rm -rf .deps/*.Po
+rm -rf .dirstamp
+rm -rf zmq_src/*.o
+rm -rf zmq_src/.deps/*.Po
+rm -rf zmq_src/.dirstamp
+rm -rf third_party/intel/*.o
+rm -rf third_party/intel/.deps/*.Po
+rm -rf third_party/intel/.dirstamp
 rm -rf secure_enclave/*.o
 rm -rf secure_enclave/.deps/*.Po
 rm -rf Makefile Makefile.in
